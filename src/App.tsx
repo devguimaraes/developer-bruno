4eab3f3d068076c4de7936466f6be668d481cc85
devguimaraes <devgmrs@gmail.com>
2025-11-23 01:32:46 -0300

    feat: aplicar Layout wrapper compartilhado nas rotas do App
    - Envolver todas as rotas com componente Layout
    - Garantir Navigation e Footer em todas as páginas
    - Manter estrutura de roteamento existente intacta
    - Aplicar layout consistente em todas as páginas do aplicativo
    - Preservar componentes de LoadingScreen e providers

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "@/components/Layout";
 import Index from "./pages/Index";
 import BlogPage from "./pages/BlogPage";
 import NotFound from "./pages/NotFound";
@@ -26,12 +27,14 @@ const App = () => {
               <Toaster />
               <Sonner />
               <BrowserRouter>
-                <Routes>
-                  <Route path="/" element={<Index />} />
-                  <Route path="/blog" element={<BlogPage />} />
-                  {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-                  <Route path="*" element={<NotFound />} />
-                </Routes>
+                <Layout>
+                  <Routes>
+                    <Route path="/" element={<Index />} />
+                    <Route path="/blog" element={<BlogPage />} />
+                    {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+                    <Route path="*" element={<NotFound />} />
+                  </Routes>
+                </Layout>
               </BrowserRouter>
             </div>
           )}
