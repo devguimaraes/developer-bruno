1813e59d8928ca7d5bed9aeac8939da3b6b1c172
devguimaraes <devgmrs@gmail.com>
2025-11-23 04:49:47 -0300

    refactor: remove componentes de loading não utilizados

--- src/components/blog/index.ts
+++ src/components/blog/index.ts
@@ -2,6 +2,5 @@
 export { BlogPostHeader } from './BlogPostHeader';
 export { BlogPostContent } from './BlogPostContent';
 export { BlogPostNavigation } from './BlogPostNavigation';
-export { BlogPostLoading } from './BlogPostLoading';
 export { BlogPostNotFound } from './BlogPostNotFound';
 export { BlogPostBackButton } from './BlogPostBackButton';
