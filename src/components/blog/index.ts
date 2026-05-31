53bc3f571964410deaa8fa696f6e961f393a7e82
Bruno Guimarães <devgmrs@gmail.com>
2026-05-30 23:49:50 -0300

    feat(wiki): add TableOfContents component

--- src/components/blog/index.ts
+++ src/components/blog/index.ts
@@ -1,8 +1,9 @@
 // Blog components exports
-export { BlogPostHeader } from './BlogPostHeader';
-export { BlogPostContent } from './BlogPostContent';
-export { BlogPostNavigation } from './BlogPostNavigation';
-export { BlogPostNotFound } from './BlogPostNotFound';
-export { BlogPostBackButton } from './BlogPostBackButton';
-export { BlogPostCardSkeleton } from './BlogPostCardSkeleton';
-export { BlogPostLoadingSkeleton } from './BlogPostLoadingSkeleton';
+export { BlogPostHeader } from "./BlogPostHeader";
+export { BlogPostContent } from "./BlogPostContent";
+export { BlogPostNavigation } from "./BlogPostNavigation";
+export { BlogPostNotFound } from "./BlogPostNotFound";
+export { BlogPostBackButton } from "./BlogPostBackButton";
+export { BlogPostCardSkeleton } from "./BlogPostCardSkeleton";
+export { BlogPostLoadingSkeleton } from "./BlogPostLoadingSkeleton";
+export { TableOfContents } from "./TableOfContents";
