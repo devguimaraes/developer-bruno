b10bad3dd4291dbc7fe090f207075379b9ccc47f
Bruno Guimarães <devgmrs@gmail.com>
2026-07-03 04:52:15 -0300

    feat(brand): add barrel export for brand components

---
+++ src/components/brand/index.ts
@@ -0,0 +1,5 @@
+// src/components/brand/index.ts
+export { BrandMascot } from "./BrandMascot";
+export type { MascotVariant, MascotState } from "./BrandMascot";
+export { BrandIcon } from "./BrandIcon";
+export type { IconName } from "./BrandIcon";
